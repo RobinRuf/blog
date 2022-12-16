4b2c1130f105d5da433835ad7aa36bd92ad56a00
Robin <robin@macbookair.home>
2022-12-16 01:28:26 +0100

    Added Sanity.io; Setting upo Sanity embedded Studio;

---
+++ app/studio/[[...index]]/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { NextStudio } from "next-sanity/studio";
+import config from "../../../sanity.config";
+
+function StudioPage() {
+  require
+  return <NextStudio config={config} />;
+}
+
+export default StudioPage;
